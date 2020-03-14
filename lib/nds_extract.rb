e8c9900123352c25893dcd0295a5375e90c00129
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-14 04:40:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -100,12 +100,12 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   result
-  i = 0 
+  i = 0
 
   while i < source.length do
     
   end
-  
+
 end
 
 # ----------------    End of Your Code Region --------------------
