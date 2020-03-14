0cbe9379e925c1a7bbe0a405f8e45d1a7b08bca1
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-14 04:38:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -80,7 +80,7 @@ def gross_per_studio(collection)
 
     if !result[movie[:studio]]
         result[movie[:studio]] = movie[:worldwide_gross]
-    else result[movie][:studio]] += movie[:worldwide_gross]
+    else result[movie][:studio] += movie[:worldwide_gross]
   end
   i += 1
 end
